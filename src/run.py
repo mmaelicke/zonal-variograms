5c8d8b061b65704d055b4534232daac082928af9
Mirko Mälicke <mirko@hydrocode.de>
2023-12-22 15:11:17 +0100

    handle tool name

--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from json2args import get_parameter
 from json2args.data import get_data_paths
@@ -11,34 +12,42 @@ def error(e: Exception):
         f.write(str(e))
         f.write('\n')
 
+toolname = os.environ.get('RUN_TOOL', 'clip')
 
 # first load the parameters
 params = get_parameter()
 
-# get the data paths
-try:
-    data_paths = get_data_paths()
-    raster_path = data_paths['raster']
-    segments_path = data_paths['segments']
-except Exception as e:
-    error(e)
-    print(str(e))
+if toolname.lower() == 'clip':
+    # get the data paths
+    try:
+        data_paths = get_data_paths()
+        raster_path = data_paths['raster']
+        segments_path = data_paths['segments']
+    except Exception as e:
+        error(e)
+        print(str(e))
+        sys.exit(1)
+
+    # build the command
+    command = []
+    if 'overlap' in params:
+        command.append('--overlap')
+    if 'crs' in params:
+        command.extend(['--crs', params['crs']])
+    if 'oid_column' in params:
+        command.extend(['--oid', params['oid_column']])
+    if 'use_oid' in params:
+        for oid in params['use_oid']:
+            command.extend(['--use-oid', oid])
+
+    # finally add the raster and segments
+    command.extend([raster_path, segments_path])
+
+    # run the click command directly
+    clip_dataset(command, standalone_mode=False)
+
+else:
+    err = Exception(f'Unknown tool: {toolname}')
+    error(err)
+    print(str(err))
     sys.exit(1)
-
-# build the command
-command = []
-if 'overlap' in params:
-    command.append('--overlap')
-if 'crs' in params:
-    command.extend(['--crs', params['crs']])
-if 'oid_column' in params:
-    command.extend(['--oid', params['oid_column']])
-if 'use_oid' in params:
-    for oid in params['use_oid']:
-        command.extend(['--use-oid', oid])
-
-# finally add the raster and segments
-command.extend([raster_path, segments_path])
-
-# run the click command directly
-clip_dataset(command, standalone_mode=False)
